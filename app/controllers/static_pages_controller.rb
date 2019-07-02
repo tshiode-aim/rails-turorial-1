d79240a186b9b0fe15c8065d9ec929654b810a26
tshiode-aim <tshiode-aim@aiming-inc.com>
2019-07-02 16:30:40 +0900

    rubocopに怒られたコードを修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+    # empty
   end
 
   def help
+    # empty
   end
 
   def about
+    # empty
   end
 
   def contact
+    # empty
   end
 end
