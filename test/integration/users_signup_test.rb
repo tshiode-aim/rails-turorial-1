98913c9ad2c975cef75161b8eb57e4c016eb6039
tshiode-aim <tshiode-aim@aiming-inc.com>
2019-07-03 17:22:52 +0900

    7.3.4 失敗時のテスト 演習

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,7 +4,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: {
+      post signup_path, params: {
         user: {
           name: "",
           email: "user@invalid",
@@ -13,6 +13,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         }
       }
       assert_template 'users/new'
+      assert_select 'div#error_explanation > ul > li', "Name can't be blank"
+      assert_select 'div#error_explanation > ul > li', "Email is invalid"
+      assert_select 'div#error_explanation > ul > li', "Password confirmation doesn't match Password"
+      assert_select 'div#error_explanation > ul > li', "Password is too short (minimum is 6 characters)"
+      assert_select 'form[action="/signup"]'
     end
   end
 end
