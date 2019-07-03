3074e206b85ed000c97f4f73cc6e6206efc5d720
tshiode-aim <tshiode-aim@aiming-inc.com>
2019-07-03 19:01:33 +0900

    8.1.3 ユーザーの検索と認証

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
+    # empty
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      ## ユーザーログインあとにユーザ情報のページにリダイレクトする
+    else
+      ## エラーメッセージを表示する
+      render 'new'
+    end
+  end
+
+  def destroy
+    # empty
   end
 end
