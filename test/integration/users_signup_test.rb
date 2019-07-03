cdb7dd1c5c220e62c63747980934472dd153431b
tshiode-aim <tshiode-aim@aiming-inc.com>
2019-07-03 18:18:27 +0900

    rubocop auto collect

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,35 +1,35 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "invalid signup information" do
+  test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
       post signup_path, params: {
         user: {
-          name: "",
-          email: "user@invalid",
-          password: "foo",
-          password_confirmation: "bar",
+          name: '',
+          email: 'user@invalid',
+          password: 'foo',
+          password_confirmation: 'bar',
         }
       }
       assert_template 'users/new'
       assert_select 'div#error_explanation > ul > li', "Name can't be blank"
-      assert_select 'div#error_explanation > ul > li', "Email is invalid"
+      assert_select 'div#error_explanation > ul > li', 'Email is invalid'
       assert_select 'div#error_explanation > ul > li', "Password confirmation doesn't match Password"
-      assert_select 'div#error_explanation > ul > li', "Password is too short (minimum is 6 characters)"
+      assert_select 'div#error_explanation > ul > li', 'Password is too short (minimum is 6 characters)'
       assert_select 'form[action="/signup"]'
     end
   end
 
-  test "valid signup information" do
+  test 'valid signup information' do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: {
         user: {
-          name: "Example User",
-          email: "user@example.com",
-          password: "password",
-          password_confirmation: "password",
+          name: 'Example User',
+          email: 'user@example.com',
+          password: 'password',
+          password_confirmation: 'password',
         }
       }
     end
