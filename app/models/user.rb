f25866089608debc81ad555c120b3a07c54ba291
tshiode-aim <tshiode-aim@aiming-inc.com>
2019-07-04 14:21:30 +0900

    9.1.1 記憶トークンと暗号化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -9,5 +10,18 @@ class User < ApplicationRecord
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+
+    # rubocop:disable Rails/SkipsModelValidations
+    ## パスワードの入力を回避するため、意図的にバリデーションをスキップする
+    update_attribute(:remember_digest, User.digest(remember_token))
+    # rubocop:enable Rails/SkipsModelValidations
   end
 end
