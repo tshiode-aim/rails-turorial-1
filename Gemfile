d130f40a72c0e8f0c306d5bd320d054ad0e6e4a0
tshiode-aim <tshiode-aim@aiming-inc.com>
2019-07-05 16:07:12 +0900

    refactor user rspec

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ end
 
 group :test do
   gem 'capybara'
+  gem 'factory_bot_rails'
   gem 'guard',                    '2.13.0'
   gem 'guard-minitest',           '2.4.4'
   gem 'minitest',                 '5.10.3'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,84 +3,106 @@ require 'spec_helper'
 
 describe User, type: :model do
   describe 'validation' do
-    before do
-      @user = User.new(
-        name: 'Example User',
-        email: 'user@example.com',
-        password: 'foobar',
-        password_confirmation: 'foobar'
-      )
-    end
-    subject { @user }
+    let(:user) { build(:user) }
+    subject { user }
 
     it 'should accept valid user' do
       is_expected.to be_valid
     end
 
-    describe 'name' do
-      it 'should reject only space' do
-        @user.name = ' '
-        is_expected.to be_invalid
-      end
+    context 'when name is only space' do
+      before { user.name = ' ' }
 
-      it 'should reject over 50 characters' do
-        @user.name = 'a' * 51
-        is_expected.to be_invalid
-      end
+      it { is_expected.to be_invalid }
     end
 
-    describe 'email' do
-      it 'should accept valid format' do
-        valid_addresses = %w(user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn)
+    context 'when name over 50 characters' do
+      before { user.name = 'a' * 51 }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context 'when email valid format' do
+      let(:valid_addresses) do
+        %w(
+          user@example.com
+          USER@foo.COM A_US-ER@foo.bar.org
+          first.last@foo.jp
+          alice+bob@baz.cn
+        )
+      end
+
+      it 'should accept all' do
         valid_addresses.each do |valid_address|
-          @user.email = valid_address
+          user.email = valid_address
           is_expected.to be_valid
         end
       end
+    end
 
-      it 'should reject only space' do
-        @user.email = ' '
-        is_expected.to be_invalid
+    context 'when email is only space' do
+      before { user.email = ' ' }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context 'when email is over 255 characters' do
+      before { user.email = 'a' * 244 + '@example.com' }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context 'when email invalid format' do
+      let(:invalid_addresses) do
+        %w(
+          user@example,com
+          user_at_foo.org
+          user.name@example.
+          foo_@bar_baz.com
+          foo@bar+baz.com
+          hoge@example..com
+        )
       end
 
-      it 'should reject over 255 characters' do
-        @user.email = 'a' * 244 + '@example.com'
-        is_expected.to be_invalid
-      end
-
-      it 'should reject invalid format' do
-        invalid_addresses = %w(user@example,com user_at_foo.org user.name@example. foo_@bar_baz.com foo@bar+baz.com hoge@example..com)
+      it 'should reject all' do
         invalid_addresses.each do |invalid_address|
-          @user.email = invalid_address
+          user.email = invalid_address
           is_expected.to be_invalid
         end
       end
+    end
 
-      it 'should be unique' do
-        duplicate_user = @user.dup
-        @user.save
-        duplicate_user.email = @user.email.upcase
-        expect(duplicate_user).to be_invalid
+    context 'when same email' do
+      before do
+        @duplicate_user = user.dup
+        user.save
+        @duplicate_user.email = user.email.upcase
       end
 
-      it 'should be saved as lower-case' do
-        mixed_case_email = 'Foo@ExAMPle.CoM'
-        @user.email = mixed_case_email
-        @user.save
-        expect(mixed_case_email.downcase).to eq @user.reload.email
+      it 'same email user should be reject' do
+        expect(@duplicate_user).to be_invalid
       end
     end
 
-    describe 'password' do
-      it 'should reject only space' do
-        @user.password = @user.password_confirmation = ' ' * 6
-        is_expected.to be_invalid
-      end
+    context 'when email with mixed case' do
+      let(:mixed_case_email) { 'Foo@ExAMPle.CoM' }
+      before { user.update_attributes(email: mixed_case_email) }
 
-      it 'should reject less than 6 characters' do
-        @user.password = @user.password_confirmation = 'a' * 5
-        is_expected.to be_invalid
+      it 'should email read from the database is lower case' do
+        expect(mixed_case_email.downcase).to eq user.reload.email
       end
+    end
+
+    context 'when password is only space' do
+      before { user.password = user.password_confirmation = ' ' * 6 }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context 'when password is less than 6 characters' do
+      before { user.password = user.password_confirmation = 'a' * 5 }
+
+      it { is_expected.to be_invalid }
     end
   end
 end
