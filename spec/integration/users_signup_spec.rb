6f9624882e9b67fca0e38e3a7e84d8ca928fa9d6
tshiode-aim <tshiode-aim@aiming-inc.com>
2019-07-08 17:48:48 +0900

    refactor RSpec v2

--- spec/integration/users_signup_spec.rb
+++ spec/integration/users_signup_spec.rb
@@ -5,21 +5,26 @@ describe 'users signup', type: :feature do
   subject { page }
 
   context 'when signup invalid information' do
-    def signup_invalid_information
-      visit signup_path
-      fill_in 'Name', with: ''
-      fill_in 'Email', with: 'user@invalid'
-      fill_in 'Password', with: 'foo'
-      fill_in 'Confirmation', with: 'bar'
-      click_on 'Create my account'
-    end
-
     it 'should not increase user count' do
-      expect { signup_invalid_information }.to change(User, :count).by(0)
+      expect {
+        visit signup_path
+        fill_in 'Name', with: ''
+        fill_in 'Email', with: 'user@invalid'
+        fill_in 'Password', with: 'foo'
+        fill_in 'Confirmation', with: 'bar'
+        click_on 'Create my account'
+      }.to change(User, :count).by(0)
     end
 
     describe 'after redirect page' do
-      before { signup_invalid_information }
+      before do
+        visit signup_path
+        fill_in 'Name', with: ''
+        fill_in 'Email', with: 'user@invalid'
+        fill_in 'Password', with: 'foo'
+        fill_in 'Confirmation', with: 'bar'
+        click_on 'Create my account'
+      end
 
       it 'should have error messages' do
         is_expected.to have_selector('div#error_explanation > ul > li', text: "Name can't be blank")
@@ -35,21 +40,26 @@ describe 'users signup', type: :feature do
   end
 
   context 'when signup valid information' do
-    def signup_valid_information
-      visit signup_path
-      fill_in 'Name', with: 'Example User'
-      fill_in 'Email', with: 'user@example.com'
-      fill_in 'Password', with: 'password'
-      fill_in 'Confirmation', with: 'password'
-      click_on 'Create my account'
-    end
-
     it 'should increase user count' do
-      expect { signup_valid_information }.to change(User, :count).by(1)
+      expect {
+        visit signup_path
+        fill_in 'Name', with: 'Example User'
+        fill_in 'Email', with: 'user@example.com'
+        fill_in 'Password', with: 'password'
+        fill_in 'Confirmation', with: 'password'
+        click_on 'Create my account'
+      }.to change(User, :count).by(1)
     end
 
     describe 'after redirect page' do
-      before { signup_valid_information }
+      before do
+        visit signup_path
+        fill_in 'Name', with: 'Example User'
+        fill_in 'Email', with: 'user@example.com'
+        fill_in 'Password', with: 'password'
+        fill_in 'Confirmation', with: 'password'
+        click_on 'Create my account'
+      end
 
       it 'should not have any error messages' do
         is_expected.to have_no_selector('div#error_explanation')
