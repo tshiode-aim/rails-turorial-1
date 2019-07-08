290aac28c9746a9be6e0df3a8ebeac1e20965f14
tshiode-aim <tshiode-aim@aiming-inc.com>
2019-07-08 16:59:59 +0900

    11.3.2 editアクションで有効化

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,14 @@
 class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update(activated: true, activated_at: Time.zone.now)
+      log_in user
+      flash[:success] = 'Account activated!'
+      redirect_to user
+    else
+      flash[:danger] = 'Invalid activation link'
+      redirect_to root_url
+    end
+  end
 end
