ba0763f984087d1a2d69448eb54a6fd5790caf8c
tshiode-aim <tshiode-aim@aiming-inc.com>
2019-07-09 13:00:20 +0900

    13.4.1 基本的な画像アップロード

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,30 +9,35 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
 
     # 無効な送信
-    assert_no_difference 'Micropost.count' do
-      post microposts_path, params: { micropost: { content: '' } }
-    end
+    post microposts_path, params: { micropost: { content: '' } }
     assert_select 'div#error_explanation'
 
     # 有効な送信
     content = 'This micropost really ties the room together'
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: {
+        micropost: {
+          content: content,
+          picture: picture
+        }
+      }
     end
-    assert_redirected_to root_url
+    assert @user.microposts.first.picture?
     follow_redirect!
     assert_match content, response.body
 
     # 投稿を削除する
-    assert_select 'a', text: 'delete'
+    assert_select 'a', 'delete'
     first_micropost = @user.microposts.paginate(page: 1).first
     assert_difference 'Micropost.count', -1 do
       delete micropost_path(first_micropost)
     end
 
-    # 違うユーザーのプロフィールにアクセス (削除リンクがないことを確認)
+    # 違うユーザーのプロフィールにアクセスする
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
   end
