669b2888254b7a9cd644f1bc2aa9f9eac2cc6607
tshiode-aim <tshiode-aim@aiming-inc.com>
2019-07-09 11:52:12 +0900

    13.3.1 マイクロポストのアクセス制御

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,11 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+    # TODO
+  end
+
+  def destroy
+    # TODO
+  end
 end
